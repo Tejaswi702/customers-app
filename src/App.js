3ea39f81a7e895456410a264073b408c0bc200ce
tejaswi <tejaswi21n6702@gmail.com>
2026-01-14 17:48:17 +0530

    Fix reset password route priority

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { supabase } from "./supabase";
+
 import Login from "./components/Login";
 import Signup from "./components/SignUp";
 import CustomerDetails from "./components/CustomersDetails";
@@ -6,17 +9,36 @@ import ForgotPassword from "./components/ForgotPassword";
 import Services from "./components/Services";
 import ResetPassword from "./components/ResetPassword";
 
+function AppRoutes() {
+  const location = useLocation();
+  const [checking, setChecking] = useState(true);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(() => {
+      setChecking(false);
+    });
+  }, []);
+
+  if (checking) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/customer-details" element={<CustomerDetails />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="/services" element={<Services />} />
+    </Routes>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/customer-details" element={<CustomerDetails />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/services" element={<Services />} />
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
